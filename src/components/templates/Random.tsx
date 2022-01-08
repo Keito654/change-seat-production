d8fa16785a9c49f2e7d29898c027a96c79ff1dce
Keito19981129 <keito19981129@gmail.com>
2022-01-08 23:32:38 +0900

    #4を修正

--- src/components/templates/Random.tsx
+++ src/components/templates/Random.tsx
@@ -36,29 +36,27 @@ const CharRandom = ({ handleRandom, tex }: CharRandomProps) => {
   return (
     <div
       style={{ textAlign: 'center' }}
-      className="h-full flex flex-col bg-white border border-gray-100 container mx-auto"
+      className="h-full flex flex-col bg-white border border-gray-100 container mx-auto my-auto p-2"
     >
-      <div className="my-auto p-2">
-        <div className="flex items-center space-x-3 lg:space-x-6 my-auto overflow-auto mx-auto border border-gray-200 h-full p-1 w-fit max-w-full">
-          {ArrTex}
-        </div>
-        <br />
-        <div className="flex flex-col">
-          <button
-            type="button"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-40 mx-auto"
-            onClick={ClickRandomButton}
-          >
-            {<PlayIcon className="h-5 w-5 inline mr-1.5" />}
-            <span className="align-middle">席替え開始！</span>
-          </button>
-          <Link href="/setting">
-            <a className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded-full mt-4 w-40 mx-auto">
-              {<CogIcon className="h-5 w-5 inline mr-1.5" />}
-              <span className="align-middle">席の設定に移動</span>
-            </a>
-          </Link>
-        </div>
+      <div className="flex items-center space-x-3 lg:space-x-6 my-auto overflow-auto mx-auto border border-gray-200  p-1 w-fit max-w-full h-full">
+        {ArrTex}
+      </div>
+      <br />
+      <div className="flex flex-col">
+        <button
+          type="button"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-40 mx-auto"
+          onClick={ClickRandomButton}
+        >
+          {<PlayIcon className="h-5 w-5 inline mr-1.5" />}
+          <span className="align-middle">席替え開始！</span>
+        </button>
+        <Link href="/setting">
+          <a className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded-full mt-4 w-40 mx-auto">
+            {<CogIcon className="h-5 w-5 inline mr-1.5" />}
+            <span className="align-middle">席の設定に移動</span>
+          </a>
+        </Link>
       </div>
     </div>
   );
